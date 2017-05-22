18e8134a6ed9a9d2e751eff194cab865e525b8ae
Jerry Lau <jerryzlau@gmail.com>
2017-05-21 20:02:51 -0700

    implemented silly_block

---
+++ lib/00_silly_blocks.rb
@@ -0,0 +1,18 @@
+def reverser
+  yield.split.map {|el| el.reverse}.join(" ")
+end
+
+def adder(n=1)
+  yield + n
+end
+
+def repeater(num=0)
+  return yield if num==0; num.times { |n| yield}
+end
+
+
+result = reverser do
+  "hello asdjkhasd"
+end
+
+p result
